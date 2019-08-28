50bdaa01b2f9d1b7596fe436ed57aeb8838b5bb2
yassh22 <yassh22@yahoo.com>
2019-08-27 21:17:23 -0400

    added more styles and javascript to the file and done.

--- js/app.js
+++ js/app.js
@@ -3,32 +3,26 @@ $(function() {
 
     $('.frontDrop').on('change', function(event) {
         event.preventDefault()
-        $('.remove').remove()
+        $('.select').remove()
         let newsInput = $(this).val()
         // console.log(newsInput)
-        $(".logoImage").css({
-            "width" : "80px",
-            "top" : "5px",
-            "height" : "80px",
-          });
-          $(".frontDrop").css({
-            "width" : "100px",
-            "top" : "40px",
-            "left" : "150px",
-          });
-          $("h1").css({
-            "top" : "17px",
-            "left" : "150px",
-          });
-          $(".wrapper").css({
-            "height" : "100px",
-          });
+     
+        $(".newResults li").remove();
 
-          $(".newResults").empty();
+        if($('footer').hasClass('footer-photos')){
+          $('footer').removeClass('footer-photos');
+        }
+
+        if(!$('header').hasClass('header-photos')){
+          $('header').addClass('header-photos');
+          $('.logoImage').addClass('logo-photos');
+        }
+
+        $('.loader').css('display', 'block');
 
         $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${newsInput}.json?api-key=Ov7TMBFdahAkreAEBSG1GAZGkw7Eznm2`)
         .done(function(data) {
-        console.log(data)
+        // console.log(data)
  
         let array = []
         for (let i=0; i < 12; i++) {
@@ -37,20 +31,31 @@ $(function() {
 
             $.each(array, function (index, value) {
 
+                let newsLink = value.url
                 let newsTitle = value.abstract
                 let picURL = this.multimedia[4].url
 
                 // create div  to hold album cover and name
-                $('.newResults').append(`<li class='news-story'><img src="${picURL}"><p class="newsCont">${newsTitle}</p></li>`).children().addClass('select')
+                $('.newResults').append(`<li class='news-story'><div class="combine"><img src="${picURL}"><a href="${newsLink}" target="_blank"><p class="newsCont">${newsTitle}</p></a></div></li>`).children().addClass('select')
                 
             })
         })
 
-            .fail(function() {
-            alert("Doesn't work hommie!!!")
-
-            })
+        .fail(function() {
+        alert("Doesn't work hommie!!!")
 
         })
 
+        .always(() => $('.loader').hide('fast'));
+
     })
+
+    // $(".newResults").on("mouseenter", "li", function(event) {
+    //     $(event.currentTarget).find(".newsCont").slideDown();
+    // });
+
+    // $(".newResults").on("mouseleave", "li", function(event) {
+    //   $(event.currentTarget).find(".newsCont").slideUp();
+    // });  
+
+})
